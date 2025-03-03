aaf3763963dac538b282537b08dbbaffc1e9c7e7
Epixu <dimomarkov@gmail.com>
2025-03-03 11:56:06 +0200

    Missing include

--- source/ImageLibrary.hpp
+++ source/ImageLibrary.hpp
@@ -7,6 +7,7 @@
 ///                                                                           
 #pragma once
 #include "Image.hpp"
+#include <Langulus/Flow/Factory.hpp>
 
 
 ///                                                                           
